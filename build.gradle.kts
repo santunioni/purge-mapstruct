9bf0e2d09754755fcde44bf585b6b01d61b93e33
Vinícius Vargas <vinicius.vargas@quintoandar.com>
2026-07-04 15:07:18 -0300

    kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("nebula.release") version "21.0.0"
     id("com.diffplug.spotless") version "8.4.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.8"
-    kotlin("jvm") version "2.1.21"
+    kotlin("jvm") version "2.4.0"
 }
 
 // Version is managed by nebula.release:
