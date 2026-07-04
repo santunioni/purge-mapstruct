c5a5c65a8ca23c59af7fd014fd5075f239613ae8
Vinícius Vargas <vinicius.vargas@quintoandar.com>
2026-07-04 13:52:35 -0300

    Rename .java to .kt

---
+++ src/main/java/io/github/santunioni/recipes/removeMapstruct/ImplementationScanner.kt
@@ -0,0 +1,28 @@
+package io.github.santunioni.recipes.removeMapstruct
+
+import io.github.santunioni.recipes.removeMapstruct.Functions.isMapperImplementation
+import org.openrewrite.ExecutionContext
+import org.openrewrite.java.JavaIsoVisitor
+import org.openrewrite.java.tree.J
+
+class ImplementationScanner(private val acc: Accumulator) : JavaIsoVisitor<ExecutionContext>() {
+    override fun visitCompilationUnit(
+        mapperImpl: J.CompilationUnit,
+        ctx: ExecutionContext,
+    ): J.CompilationUnit {
+        if (!isMapperImplementation(mapperImpl)) {
+            return mapperImpl
+        }
+
+        for (classDecl in mapperImpl.classes) {
+            if (mapperImpl.packageDeclaration == null) continue
+
+            for (interfaceDecl in classDecl.implements.orEmpty()) {
+                acc.addLinking(interfaceDecl, mapperImpl)
+            }
+
+            classDecl.extends?.let { acc.addLinking(it, mapperImpl) }
+        }
+        return super.visitCompilationUnit(mapperImpl, ctx)
+    }
+}
